9b359a278210a92af908e1ad43a830a94f42163f
Sophearith Saing <sophearithsaing123@gmail.com>
2022-12-03 23:56:56 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+// Development log
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Body parser
+app.use(express.json({ limit: '10kb' }));
+
+// Serve static files
+app.use(express.static(`${__dirname}/public`));
+
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (error) => {
+  console.log('Uncaught exception! Shutting down...');
+  console.log(error.name, error.message);
+  process.exit(1);
+});
+
+dotenv.config({
+  path: './config.env',
+});
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
+
+const app = require('./app');
+
+// Start server
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (error) => {
+  console.log('Unhandled rejection! Shutting down...');
+  console.log(error.name, error.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
