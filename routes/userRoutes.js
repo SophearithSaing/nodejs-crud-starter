eb9f453c00fab0627d9f2b2ede2d733ff08664cf
Sophearith Saing <sophearithsaing123@gmail.com>
2022-12-04 13:38:34 +0700

    Add user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.patch(
+  '/updatePassword',
+  authController.protect,
+  authController.updatePassword
+);
+
+router.patch(
+  '/updateCurrentUser',
+  authController.protect,
+  userController.updateCurrentUser
+);
+
+router.delete(
+  '/deleteCurrentUser',
+  authController.protect,
+  userController.deleteCurrentUser
+);
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+module.exports = router;
