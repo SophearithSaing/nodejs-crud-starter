dcc39e6da040a3eab64f01a36384b79cf3f05abc
Sophearith Saing <sophearithsaing123@gmail.com>
2022-12-04 15:24:46 +0700

    Add routes to app.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const sampleDataRouter = require('./routes/sampleDataRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
@@ -19,6 +22,15 @@ app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
+});
+
+// Routes
+app.use('/api/v1/sample-data', sampleDataRouter);
+app.use('/api/v1/users', userRouter);
+
+// Handle unmatched route
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 module.exports = app;
